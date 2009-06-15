5a2ff5094aed49de3ff4ab31413f0d8e1276c1ae
David Cournapeau <cournape@gmail.com>
2009-06-15 16:02:35 +0900

    Handle nan and inf in gamma unit tests.

--- scipy/special/scipyfunc/tests/test_gamma.py
+++ scipy/special/scipyfunc/tests/test_gamma.py
@@ -4,8 +4,8 @@ import numpy as np
 from numpy.testing import *
 
 from scipy.special.scipyfunc.scipyfunc import gamma
-#from scipy.special import gamma
-from scipy.special.scipyfunc.tests.common import parse_txt_data, assert_tol_equal
+from scipy.special.scipyfunc.tests.common import parse_txt_data, \
+    assert_tol_equal, nulps_array
 
 datadir = join(dirname(__file__), "data")
 
@@ -20,7 +20,21 @@ class _TestGamma(TestCase):
 
         y = gamma(x)
         self.failUnless(y.dtype == self.dt)
-        assert_tol_equal(y, y_r, atol=self.atol, rtol=self.rtol)
+
+        # Check inf/nan values in ref and computed values coincide, and discard
+        # them from assert_tol_equal
+        id = np.isnan(y)
+        id_r = np.isnan(y_r)
+        np.testing.assert_array_equal(id, id_r)
+
+        id = np.isinf(y)
+        id_r = np.isinf(y_r)
+        np.testing.assert_array_equal(id, id_r)
+        np.testing.assert_array_equal(y[id], y_r[id])
+
+        id = np.isfinite(y)
+        id_r = np.isfinite(y_r)
+        assert_tol_equal(y[id], y_r[id], atol=self.atol, rtol=self.rtol)
 
     def test_factorials(self):
         self._test("factorials.txt")
@@ -57,6 +71,9 @@ class TestGammaSingle(_TestGamma):
     dt = np.float32
     atol = 1e-6
     rtol = 1e-6
+    @dec.knownfailureif(True, "Loss of precision near 0 in single precision")
+    def test_near_0(self):
+        self._test("near_0.txt")
 
 class TestGammaLongExtended(_TestGamma):
     dt = np.longdouble

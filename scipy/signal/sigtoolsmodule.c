9fc8d19579d8ebc154246ec230482c34b3fb662f
David Cournapeau <cournape@gmail.com>
2009-07-09 13:02:52 +0900

    Fix calls to PyArray_IterNew in convolve2d.
    The calls were mistakenly taking PyObject pointers instead of
    PyArrayObject pointers.

--- scipy/signal/sigtoolsmodule.c
+++ scipy/signal/sigtoolsmodule.c
@@ -1400,12 +1400,12 @@ static PyObject *sigtools_convolve2d(PyObject *NPY_UNUSED(dummy), PyObject *args
     flag = mode + boundary + (typenum << TYPE_SHIFT) + \
       (flip != 0) * FLIP_MASK;
 
-itSignal = (PyArrayIterObject* )PyArray_IterNew((PyObject*)in1);
+itSignal = (PyArrayIterObject* )PyArray_IterNew((PyObject*)ain1);
     if (itSignal == NULL) {
         return NULL;
     }
 
-itKernel = (PyArrayIterObject* )PyArray_IterNew((PyObject*)in2);
+itKernel = (PyArrayIterObject* )PyArray_IterNew((PyObject*)ain2);
 	if (itKernel == NULL) {
 		return NULL;
   }

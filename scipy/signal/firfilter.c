baaca83e1e2e848c65acfdb79ae977126f2eb3db
Ross Smith (gaurdro) <gaurdro@gmail.com>
2009-07-10 17:50:33 -0400

    new convolve2d.  builds fine, not tested.

--- scipy/signal/firfilter.c
+++ scipy/signal/firfilter.c
@@ -4,6 +4,7 @@
 #include <numpy/noprefix.h>
 
 #include "sigtools.h"
+#define PYERR(message) {PyErr_SetString(PyExc_ValueError, message);}
 static int elsizes[] = {sizeof(Bool),
 						sizeof(byte),
                         sizeof(ubyte),
@@ -86,85 +87,33 @@ static OneMultAddFunction *OneMultAdd[]={NULL,
 
 /* This could definitely be more optimized... */
 
-int pylab_convolve_2d(PyArrayIterObject* itSignal,
-			intp* Signals,
-			PyArrayIterObject* itKernel,
-			intp* Kerns,
-			PyArrayIterObject* itOut,
-			intp* Outs,
-			int flag,
-			char  *fillvalue)
-{
-  int boundary, outsize, convolve, type_size, type_num;
-  int i,j;
-  PyArrayNeighborhoodIterObject *curSignal;
-  int bounds[4];
+void convolve2d_worker(PyArrayIterObject *itSignal, 
+			 		   PyArrayNeighborhoodIterObject *curSignal, 
+			 		   PyArrayIterObject *itKern, 
+			 		   PyArrayNeighborhoodIterObject *curKern, 
+			 		   PyArrayIterObject *itOut, 
+			 		   int typenum,
+			 		   int mode, 
+			 		   int boundary)
+  {
   OneMultAddFunction *mult_and_add;
-char *sum=NULL, *value=NULL;
-
-
-  boundary = flag & BOUNDARY_MASK;  /* flag can be fill, reflecting, circular */
-  outsize = flag & OUTSIZE_MASK;
-  convolve = flag & FLIP_MASK;
-  type_num = (flag & TYPE_MASK) >> TYPE_SHIFT;
+  int i, j;
   
-
-  mult_and_add = OneMultAdd[type_num];
-  if (mult_and_add == NULL) return -5;  /* Not available for this type */
-
-  if (type_num < 0 || type_num > MAXTYPES) return -4;  /* Invalid type */
-  type_size = elsizes[type_num];
-
-  if ((sum = calloc(type_size,2))==NULL) return -3; /* No memory */
-  value = sum + type_size;
-
- /*need to setup neighbor hood iterator over signal*/
-  switch (outsize){
-
-  case (VALID):
-	bounds[0] = convolve ? (Kerns[0]-1) : 0;
-	bounds[2] = convolve ? (Kerns[1]-1) : 0;
-	break;
-  case (SAME):
-	bounds[0] = convolve ? (((Kerns[0]-1)>>1)) : (((--Kerns[0]-1) >> 1));
-	bounds[2] = convolve ? (((Kerns[1]-1)>>1)) : (((--Kerns[1]-1) >> 1));
-	break;
-  case (FULL):
-	bounds[0] = convolve ? 0 : (0-Kerns[0]+1);
-	bounds[2] = convolve ? 0 : (0-Kerns[1]+1);
-	break;
-  default:
-	return -1;
-}
-  bounds[1] = bounds[0];
-  bounds[3] = bounds[2];
-  if (convolve) {
-  	bounds[0] +=Outs[0];
-  	bounds[2] +=Outs[1];
- }
-  else{
-  	bounds[1] += Outs[0];
-  	bounds[3] += Outs[1];
- }
+  mult_and_add = OneMultAdd[typenum];
+  if (mult_and_add == NULL) {PYERR("Convolve not available for this type");}
   
-  curSignal = (PyArrayNeighborhoodIterObject *)PyArray_NeighborhoodIterNew(itSignal, bounds);
-  
-if (boundary == VALID){
-  for (i = 0; i<itOut->size;++i){/*iterate over each element in the output array*/
-	memset(sum, 0, type_size);
-	PyArray_ITER_NEXT((PyObject *)itOut);
-	for (j = 0; i<itKernel->size;++j){
-		PyArray_ITER_NEXT((PyObject *)itKernel);
-		mult_and_add(sum,(char *)itKernel,(char *)curSignal);
+  for (i = 0;i<itOut->size;++i){
+  	for (j = 0; j < curSignal->size;++j){
+  		mult_and_add((char *)itOut,(char *)curSignal,(char *)curKern);
+  		
+  		PyArrayNeighborhoodIter_Next(curSignal);
+  		PyArrayNeighborhoodIter_Next(curKern);
 	}
-
-	//memcpy(itOut, sum, type_size);
-	PyArray_ITER_RESET((PyObject *)itKernel);
-  }
+	PyArray_ITER_NEXT(itSignal);
+	PyArrayNeighborhoodIter_Reset(curSignal);
+	PyArrayNeighborhoodIter_Reset(curKern);
 }
-
-return 0;
-
+  return ;
 }
 
 
